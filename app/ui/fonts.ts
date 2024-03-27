329abd6e1d7be8ef2198cc5269e5d52cdfcdc733
Guido.R <guidorivoira@hotmail.com>
2024-03-27 16:39:48 -0300

    agrega lusitana font

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,4 +1,5 @@
-import { Inter, Lora } from 'next/font/google';
+import { Inter, Lora, Lusitana } from 'next/font/google';
  
 export const inter = Inter({ subsets: ['latin'] });
 export const lora = Lora({ subsets: ['latin'], style: ['italic', 'normal'] ,weight: ['400', '700'] });
+export const lusitana = Lusitana({ subsets: ['latin'], weight: ['400', '700'] });
